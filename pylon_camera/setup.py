7b064893a01d2751e26eef1a8bd97ebc5c1c3da8
l1sa3 <lisaengel2502@gmail.com>
2026-03-02 10:23:17 +0100

    Start ROS2 migration

--- pylon_camera/setup.py
+++ pylon_camera/setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     # #  don't do this unless you want a globally visible script
